8b255b8de6c806251d007618afb0dcdcbff4e100
Andrew.OSUServer <wilsoan6@oregonstate.edu>
2019-08-05 16:57:11 -0700

    working on setting up data connection

--- Project 2/ftserver.c
+++ Project 2/ftserver.c
@@ -12,6 +12,7 @@
 #include <netdb.h>
 #include <stdlib.h>
 #include <netinet/in.h> //For the AF_INET (Address Family)
+#include <dirent.h> //for directory listing
 
 void setupConnection(int * fd, char * port){
 
@@ -23,6 +24,7 @@ void setupConnection(int * fd, char * port){
     hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+    
     
     getaddrinfo(NULL, port, &hints, &res);
 
@@ -41,16 +43,60 @@ void setupConnection(int * fd, char * port){
     }
 }
 
+// adapted from: https://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/
+void printDirectoryContentsToString(char contents[]){
+    struct dirent *de; // Pointer for directory entry
+    char name[64];
+
+    // opendir() returns a pointer of DIR type.
+    DIR *dr = opendir(".");
+
+    if (dr == NULL) // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open current directory");
+        return 0;
+    }
+
+    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html for readdir()
+    while ((de = readdir(dr)) != NULL)
+    {
+        if(!(strcmp(de->d_name, "..") == 0 || strcmp(de->d_name, ".") == 0))
+        {
+            sprintf(name, "%s\n", de->d_name);
+            strcat(contents, name);
+        }
+    }
+    //printf("%s", contents);
+
+    closedir(dr);
+}
+
+void sendDirectoryContents(){
+    char contents[1000];
+    memset(contents, "\0", 1000);
+    int bytesSent, messageLength;
+
+    printDirectoryContentsToString(contents);
+    messageLength = strlen(contents);
+
+    bytesSent = send(, contents, messageLength, 0);
+
+}
+
 void handleRequest(int controlConnectionFD){
     int dataConnectionFD;
-    int numCommands = 0;
     struct sockaddr_storage dataAddress;
     socklen_t dataAdrSize;
-    char * commands[5]; //used to store commands from client. There should be a max of 3, but added extra just to be safe
-    char * token;
     char buffer[1028];
-    memset(buffer, "\0", sizeof(buffer));
+    char clientPort [128];
+    char * commands[5]; //used to store commands from client. There should be a max of 3, but added extra just to be safe
+    int numCommands = 0;
+    char * token;
 
+    memset(buffer, "\0", sizeof(buffer));
+    memset(clientPort, "\0", sizeof(clientPort));
+
+    //recieve commands from client into buffer
     recv(controlConnectionFD, buffer, sizeof(buffer), 0);
 
     //token is null when string is empty, so this will loop over entire string
@@ -62,6 +108,22 @@ void handleRequest(int controlConnectionFD){
         numCommands++;
         token = strtok(NULL, " ");
     }
+
+    if(strcmp(commands[0], "-l") == 0){
+        // printf("List directory requested on port %d", clientPort);
+        // printf("Sending directory contents to %s:%d", clientHost, clientPort);
+        strcopy(clientPort, commands[1]); //commands should be "-l <port>"
+        sendDirectoryContents();
+    }
+    else if( strcmp(commands[0], "-g")==0){
+        strcopy(clientPort, commands[2]); //commands should be "-g <filename> <port>"
+        //send file
+    }
+    else{
+        //error
+    }
+    close(dataConnectionFD);
+    close(controlConnectionFD);
 }
 
 int main(int argc, char *argv[]){
@@ -83,7 +145,7 @@ int main(int argc, char *argv[]){
     while(1){
         ctrlAdrSize = sizeof(controlAddress);
         controlConnectionFD = accept(serverFD, (struct sockaddr *)&controlAddress, &ctrlAdrSize);
-        printf("connected to something");
+        printf("Connection from somewhere");
 
         handleRequest(controlConnectionFD);
     }
